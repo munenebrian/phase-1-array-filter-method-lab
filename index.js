5b49b70607304baf460421139505c77fff3547ad
munenebrian <njerubrianmunene@gmail.com>
2022-06-11 13:03:49 +0300

    commit tree 0a7535eb7a3bf280fd561820c4f94f7560c3402a

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(name, drivers) {
+    return name.filter(
+        (possibleMatch) => possibleMatch.toLowerCase() === drivers.toLowerCase()
+    );
+}
+
+function fuzzyMatch(name, drivers) {
+    return name.filter(
+        (possibleMatch) =>
+        possibleMatch.toLowerCase().indexOf(drivers.toLowerCase()) === 0
+    );
+}
+
+function matchName(name, soughtDrivers) {
+    return name.filter((record) => record.name === soughtDrivers);
+}
